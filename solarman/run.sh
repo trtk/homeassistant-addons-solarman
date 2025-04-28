930b6d6e861fb90e8f378e0a06a98a229bee8657
Richard Toth <trtk1992@gmail.com>
2025-04-28 12:57:46 +0200

    Update run.sh

--- solarman/run.sh
+++ solarman/run.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-echo "Starting Solarman MQTT Bridge..."
+echo "Creating config.json..."
 
-# Elindítjuk az alkalmazást a megfelelő paraméterekkel
-python3 solarmanmqtt.py \
-  --mqtt_broker "$mqtt_broker" \
-  --mqtt_username "$mqtt_username" \
-  --mqtt_password "$mqtt_password" \
-  --sn "$solarman_sn"
+cat <<EOF > /config.json
+{
+  "name": "${name}",
+  "url": "${url}",
+  "appid": "${appid}",
+  "secret": "${secret}",
+  "username": "${username}",
+  "password": "${password}",
+  "orgId": "${orgId}",
+  "stationId": ${stationId},
+  "inverterId": "${inverterId}",
+  "loggerId": ${loggerId},
+  "interval": ${interval},
+  "debug": ${debug},
+  "mqtt": {
+    "broker": "${mqtt_broker}",
+    "port": ${mqtt_port},
+    "topic": "${mqtt_topic}",
+    "username": "${mqtt_username}",
+    "password": "${mqtt_password}"
+  }
+}
+EOF
+
+echo "Starting Solarman MQTT Bridge..."
+python3 solarmanmqtt.py --config /config.json
